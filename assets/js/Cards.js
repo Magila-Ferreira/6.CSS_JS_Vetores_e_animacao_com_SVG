cedbaaa3725eb5d9cc2472a6a95706631a99111c
Mágila-Ferreira <magilamorganasf@hotmail.com>
2021-08-19 10:35:32 -0300

    18/08 - Animando os cards

--- assets/js/Cards.js
+++ assets/js/Cards.js
@@ -1,10 +1,21 @@
 class Cards {
     constructor() {
+        this.posicaoSectionDicas = document.getElementById('section-dicas');
+        this.cardEsq = document.querySelector('.card-esq');
+        this.cardDir = document.querySelector('.card-dir');
 
     }
 
-    teste() {
-        console.log('Funcionou!!!')
+    scrollCards() {
+        window.requestAnimationFrame(this.calculoScroll.bind(this));
+    }
+
+    calculoScroll() {
+        const posicao = this.posicaoSectionDicas.getBoundingClientRect()['y'];
+        if (posicao >= 25) {
+            this.cardEsq.style.transform = `translate(${((-posicao) + 25) / 10}%)`;
+            this.cardDir.style.transform = `translate(${(posicao - 25) / 10}%)`;
+        }
     }
 }
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
-import { Cards } from './Cards.js'
+import { Cards } from './Cards.js';
+import { Icones } from './Icones.js';
 
 window.onload = () => {
-    let animaCards = new Cards()
-    document.addEventListener('scroll', animaCards.teste)
-}
+    const animaCards = new Cards()
+    document.addEventListener('scroll', animaCards.scrollCards.bind(animaCards));
+
+    const icones = new Icones();
+    icones.animaIcones();
+};
+
